f82edd9dc7aa928dee845b5ae2181ab1b729553f
Helena Copppieters <hcoppieters@hotmail.com>
2020-12-08 16:53:19 +0100

    Add project files.

---
+++ Models/Factuur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_Boekhouding
+{
+    public class Factuur
+    {
+        public string UniekNr { get; set; }
+        public DateTime FactuurDatum { get; set; }
+        public double BedragExclBTW { get; set; }
+        public int BTWTarief { get; set; }
+        public string Status { get; set; }
+        public double BedragInclBTW { get { return BedragExclBTW + BTWBedrag; } }
+        public string Omschrijving { get; set; }
+        public DateTime BetaalDatum { get; set; }
+        public double BTWBedrag { get { return BedragExclBTW * BTWTarief / 100.0; }  }
+        public DateTime VervalDatum { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
---
+++ Services/IBoekhoudingDataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Demo_Boekhouding.ViewModels;
+
+namespace Demo_Boekhouding.Services
+{
+    public interface IBoekhoudingDataService
+    {
+        IList<AankoopFactuur> GeefAankoopDagboek();
+        IList<KasVerrichting> GeefKasboek();
+        IList<VerkoopFactuur> GeefVerkoopDagboek();
+        IList<AankoopFactuur> VerwijderAankoopFactuur(AankoopFactuur factuur);
+        IList<AankoopFactuur> VoegAankoopFactuurToe(AankoopFactuur factuur);
+        void WijzigAankoopFactuur(AankoopFactuur nieuwefactuur);
+        IList<Klant> GeefAlleKlanten();
+        IList<Leverancier> GeefAlleLeveranciers();
+        //verder aan te vullen
+    }
+}
